e33a3ef0cbfc6e2477ce7b6cb8a65dac0a131f46
Davi P. Oliveira <dpo.dev11@gmail.com>
2025-07-26 21:46:46 -0300

    added TSXRules.tsx

---
+++ src/basics/exercises/03-tsx-rules/TSXRules.tsx
@@ -0,0 +1,11 @@
+const TSXRules: React.FC = () => {
+  return (
+    <p>
+        - JSX must return a **single parent element**.
+        - JSX elements must be **properly closed**.
+        - JSX attributes are written using **camelCase** (e.g., `className` instead of `class`).
+    </p>
+  )
+}
+
+export default TSXRules
